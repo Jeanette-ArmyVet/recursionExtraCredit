9e9e50ca66bf4bd12710f514f12af67743bc6936
Jeanette-ArmyVet <jeanette.mrtnz@gmail.com>
2023-04-26 20:54:30 -0700

    triple quote comment added

--- RecursionExtraCredit.py
+++ RecursionExtraCredit.py
@@ -1,4 +1,9 @@
 def power(x, n):
+    """
+    Computes the value of x raised to the power of n, using recursion.
+    Assumes that n is an even and non-negative integer.
+    """
+    
     if n == 0:
         return 1
     else:
